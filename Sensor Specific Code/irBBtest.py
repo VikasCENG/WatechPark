960cf3d5b7a100c9cb24e1061ec5fcb4edad0c80
VikasCENG <55265964+VikasCENG@users.noreply.github.com>
2020-02-09 18:48:56 -0500

    Add files via upload

---
+++ Sensor Specific Code/irBBtest.py
@@ -0,0 +1,35 @@
+#########
+#File: irBBtest.py
+#Date: Sunday Nov 03, 2019   22:32 PM
+#Author: George Alexandris
+#Purpose: Going to implement uploading data to the firebase database
+########
+
+import time
+import RPi.GPIO as GPIO
+
+Gpio_Pin=10
+
+#setup GPIO pin as input to read value for HIGH or LOW
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Gpio_Pin,GPIO.IN)
+
+try:
+	while True:
+		x = GPIO.input(Gpio_Pin)
+		if(x==1):
+			print("Solid,LED OFF")
+		if(x==0):
+        		print("Beam Broken,LED ON")
+		
+		time.sleep(2.0)		
+	
+except KeyboardInterrupt:
+	print("CTRL+C clicked")
+
+except:
+	print("some error occurred")
+
+finally:
+	GPIO.cleanup()
+
